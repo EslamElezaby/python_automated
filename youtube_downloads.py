55c4525d0f0d3972db48de551653c018e203f969
EslamElezaby <mme367254@gmail.com>
2021-01-18 13:42:36 +0200

    refomat

--- youtube_downloads.py
+++ youtube_downloads.py
@@ -35,23 +35,23 @@ video = YouTube(link)
 # print(video.streams.get_highest_resolution())
 # print(video.streams.get_lowest_resolution())
 
-## Finally to download video
-# selected_path = "E:/"
-# def finish():
-#     path = os.path.realpath(selected_path)
-#     os.startfile(path)
-#     print('Download Done')
+# Finally to download video
+selected_path = "E:/"
+def finish():
+    path = os.path.realpath(selected_path)
+    os.startfile(path)
+    print('Download Done')
 
 
-# ## After download will automated oping folder you seleted
-# video.streams.get_lowest_resolution().download(output_path=selected_path)
-# video.register_on_complete_callback(finish())
+## After download will automated oping folder you seleted
+video.streams.get_lowest_resolution().download(output_path=selected_path)
+video.register_on_complete_callback(finish())
 
 ## download playlist
-playlist_link = 'https://www.youtube.com/playlist?list=PLDN8vU_ZehsUrlkJ34Rdbsu1VduPvLvvZ'
-Playlist = Playlist(playlist_link)
-for video in Playlist.videos:
-    video.streams.get_lowest_resolution().download(output_path="E:/")
+# playlist_link = 'https://www.youtube.com/playlist?list=PLDN8vU_ZehsUrlkJ34Rdbsu1VduPvLvvZ'
+# Playlist = Playlist(playlist_link)
+# for video in Playlist.videos:
+#     video.streams.get_lowest_resolution().download(output_path="E:/")
 
 ## for more information about pytube visit
 # link => https://python-pytube.readthedocs.io/en/latest/index.html    
