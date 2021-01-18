8d7740499bda454a312a1a166fe7515d914bed06
EslamElezaby <mme367254@gmail.com>
2021-01-18 12:14:04 +0200

    start coding

--- youtube_downloads.py
+++ youtube_downloads.py
@@ -1,1 +1,19 @@
 ## create youtube download using python
+
+# 1# install git
+# 2# install pytube package
+# write this line i your command
+# pip install pytube
+
+
+## let's coding....
+from pytube import YouTube
+link = 'https://youtu.be/85hKAIMJ6Yk'
+
+video = YouTube(link)
+
+
+## To Know info about video like {title , description , length, rating, views}
+print(f"Video Title :\n{video.title}\n_________________________________________________")
+print(f"Video Description :\n{video.description}\n_____________________________________")
+print(f"Video Length :\n {video.length}\n_________________________________________________")
